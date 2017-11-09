5bfcce4f3250afe75dafd3391631d60876be531b
alekslesik <alekslesik@gmail.com>
2017-11-09 22:39:33 +0300

    initial template_bootstrap

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+ var gulp        = require('gulp'),
+	sass        = require('gulp-sass'),
+	browserSync = require('browser-sync'),
+	concat      = require('gulp-concat'),
+	uglify      = require('gulp-uglifyjs');
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false
+	});
+});
+
+gulp.task('watch', ['browser-sync',], function() {
+	//gulp.watch('app/sass/**/*.sass', ['sass']);
+	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/js/**/*.js', browserSync.reload);
+	gulp.watch('app/css/*.css', browserSync.reload);
+});
